1ee8e96c25e72f37fa5e034cf184c2743ec7a161
Brandon Brasson <rocksong622@gmail.com>
2017-10-12 17:19:56 +0000

    automatically backed up by learn

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,5 +1,8 @@
 describe "Hello World" do 
 
+  $ ruby hello_world.rb
+  Hello World!
+
   it 'puts "Hello World!"' do
     # $stdout represents the output of your program, don't worry about how
     # we are testing this, focus on simply coding your solution.
